68de8a7fa75d7fbd2dcebc626d23024740a713b3
AlexS <ashilov84@ya.ru>
2022-09-29 21:40:14 +0500

    Добавил автофокус и фокус после отправки сообщения

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,22 +1,16 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import './App.css';                                                             //импортируем App.css
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
 import './StyleApp.css';
-//из material-ui для полей ввода "пользователь (author)" и "сообщение (text)"
-import Box from '@mui/material/Box';
+//Material UI
 import TextField from '@mui/material/TextField';
-//из material-ui для кнопки "отправить"
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+
 
 function App() {
-  const superMess = [
-    { author: 'Jon', text: "Hi! What's up?" },
-    { author: 'Ken', text: "Normal. I'm going to school." },
-    { author: 'Mery', text: 'Are you out of your mind? Today is the day off!' }
-
-  ]
-  const [messageList, setMessageList] = useState(superMess);
+  const [messageList, setMessageList] = useState([]);
   const [messageBody, setMessageBody] = useState({ text: '', author: '' });
   const ROBOT_MESSAGE = 'Привет человет! Я получил твое сообщение';
 
@@ -30,7 +24,6 @@ function App() {
 
   return (
     <div className='App'>
-      <Button variant="contained">Contained</Button>
       <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
       <div className='massageList'>
         {messageList.map((e, i) => <Message text={e.text} author={e.author} key={i} />)}
@@ -43,30 +36,28 @@ export default App;
 
 const Form = ({ data, setData, setMessage }) => {
 
+  const inputFocus = useRef(null);
+
   const { text, author } = data;
 
   const submitForm = (item) => {
-    console.log(item)
-    item.preventDefault()
     if (text.length > 0) {
       setMessage(prevent => [...prevent, { text, author }])
-      console.log(setMessage)
+      item.preventDefault();
     }
-
     setData({ text: '', author: '' })
+    inputFocus.current.childNodes[1].childNodes[0].focus()
   }
 
   return (
     <form onSubmit={submitForm}>
-      <input placeholder="Имя" value={author} onChange={(item) => {
+      <TextField fullWidth ref={inputFocus} label="Имя" id="fullWidth" autoFocus={true} value={author} onChange={(item) => {
         setData(pervstate => ({ ...pervstate, author: item.target.value }));
       }} />
-
-      <input placeholder="Текст" value={text} onChange={(item) => {
+      <TextField fullWidth label="Сообщение" id="fullWidth" value={text} onChange={(item) => {
         setData(pervstate => ({ ...pervstate, text: item.target.value }));
       }} />
-
-      <button type='submit'>Отправить</button>
+      <Button type='submit' variant="contained" endIcon={<SendIcon />} > Send </Button>
     </form>
   )
 }
@@ -82,38 +73,3 @@ const Message = ({ author, text }) => {
     </div>
   )
 }
-
-/* Код material-ui для вставки простого input:
-
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-export default function FullWidthTextField() {
-  return (
-    <>
-      <Box sx={{ width: 500, maxWidth: '100%', }}>
-        <TextField fullWidth label="Имя" id="fullWidth" />
-      </Box>
-      <Box sx={{ width: 500, maxWidth: '100%', }}>
-        <TextField fullWidth label="Сообщение" id="fullWidth" />
-      </Box>
-    </>
-  );
-}
-*/
-
-/*Код material-ui для вставки простой кнопки
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-
-export default function BasicButtons() {
-  return (
-    <Stack spacing={2} direction="row">
-      <Button variant="contained">Contained</Button>    //Вариант 1
-      <Button variant="outlined">Outlined</Button>      //Вариант 2
-    </Stack>
-  );
-}
-*/
