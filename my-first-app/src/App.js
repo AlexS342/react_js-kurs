ab6f69d49193d9fff89da826ef14e4f7da1b44c9
AlexS <ashilov84@ya.ru>
2022-09-30 22:06:19 +0500

    Удалил закоментированные импорты реакта

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -6,18 +6,11 @@ import './StyleApp.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-//import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-//import Typography from '@mui/material/Typography';
-
-//import List from '@mui/material/List';
-//import ListItem from '@mui/material/ListItem';
-//import ListItemText from '@mui/material/ListItemText';
 import CommentIcon from '@mui/icons-material/Comment';
 import IconButton from '@mui/material/IconButton';
 
