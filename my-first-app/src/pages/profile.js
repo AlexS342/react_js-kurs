5054f6a9b77b460dd59f48baa311e9e79fa21411
AlexS <ashilov84@ya.ru>
2022-10-01 23:50:07 +0500

    Один компонент в качестве заглушки

---
+++ my-first-app/src/pages/profile.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+
+function Profile() {
+
+    return (
+        <div>
+            <h1>Добро пожалоть</h1>
+            <p>Это страница вашего профиля</p>
+            <p>Возможно, что скоро вы его сможите заполнить</p>
+        </div>
+    )
+}
+
+export default Profile;
