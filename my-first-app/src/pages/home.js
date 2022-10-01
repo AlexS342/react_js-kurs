315dbce1fcb5893de9f8537aec02104870d21ade
AlexS <ashilov84@ya.ru>
2022-10-01 23:49:30 +0500

    Один компанент для приветствия!

---
+++ my-first-app/src/pages/home.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Добро пожалоть</h1>
+            <p>Это главная страница</p>
+            <p>Она пока пустая, но это временно</p>
+        </div>
+    )
+}
+
+export default Home;
